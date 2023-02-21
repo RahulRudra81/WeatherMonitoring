54d0b885df40156ee9c150724454a980c299de91
Rahul Rudra <91683510+RahulRudra81@users.noreply.github.com>
2023-02-22 01:36:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,22 +5,27 @@ const requests=require("requests")
 const homeFile=fs.readFileSync("home.html","utf-8")
 
 const replaceVal=(tempVal,orgVal)=>{
-    let temperature=tempVal.replace("{%tempval%}",orgVal.main.temp)
-    temperature=tempVal.replace("{%tempmin%}",orgVal.main.temp_min)
-    temperature=tempVal.replace("{%tempmax%}",orgVal.main.temp_max)
-    temperature=tempVal.replace("{%location%}",orgVal.name)
-    temperature=tempVal.replace("{%country%}",orgVal.sys.country)
+    var temps=Math.ceil(orgVal.main.temp-273);
+    var tempsmax=Math.ceil(orgVal.main.temp_max-273);
+    var tempsmin=Math.ceil(orgVal.main.temp_min-273);
+    var temperature=tempVal.replace("{%tempval%}",temps)
+    .replace("{%tempmin%}",tempsmin)
+    .replace("{%tempmax%}",tempsmax)
+    .replace("{%location%}",orgVal.name)
+    .replace("{%country%}",orgVal.sys.country)
+    .replace("{%tempStatus%}",orgVal.weather[0].main)
     return temperature;
-};
+}; 
 const server=http.createServer((req,res)=>{
     if(req.url=="/"){
         requests(
            "https://api.openweathermap.org/data/2.5/weather?q=Bangalore&appid=e41a56c606f47d2415bb50a54190030c"
         )
-        .on('data', (chunk)=>{
+        .on("data", (chunk)=>{
             const objdata=JSON.parse(chunk)
-            const arrData=[objdata]
+            const arrData=[objdata];
             const realTimeData=arrData.map((val)=> replaceVal(homeFile,val)).join("");
+            // console.log(objdata)
             res.write(realTimeData)
         })
          .on('end',(err) => {
